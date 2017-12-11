2b2748358c550ea81b348fb357ca47b9776864e4
Zeeko <vaezt@outlook.com>
2017-12-11 22:53:51 +0800

    更正命名问题

--- ZeekoBlog/Filters/JwtAuthorizeAttribute.cs
+++ ZeekoBlog/Filters/JwtAuthorizeAttribute.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
 namespace ZeekoBlog.Filters
 {
-    public class JwtAuthorize:AuthorizeAttribute
+    public class JwtAuthorizeAttribute : AuthorizeAttribute
     {
-        public JwtAuthorize()
+        public JwtAuthorizeAttribute()
         {
             AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme + "," +
                                     JwtBearerDefaults.AuthenticationScheme;
