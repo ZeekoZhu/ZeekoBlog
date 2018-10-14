33cc9f6a972b5e6c1277849112d2029f1850a299
Zeeko <vaezt@outlook.com>
2018-10-14 13:42:00 +0800

    ⬆️ Upgrade webpck to 4.0

--- ZeekoBlog/webpack.config.js
+++ ZeekoBlog/webpack.config.js
@@ -1,15 +1,16 @@
 /// <binding BeforeBuild='Run - Development' />
 'use strict';
-const webpack = require('webpack');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-let isProd = process.env.ASPNETCORE_ENVIRONMENT === 'Production' || process.env.NODE_ENV === 'production';
+let isProd = (process.env.ASPNETCORE_ENVIRONMENT && process.env.ASPNETCORE_ENVIRONMENT.toLowerCase() === 'production') || (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production');
+
 
 let entries = {
-    'zeeko.js': './wwwroot/ts/Zeeko.ts',
-    'article.js': './wwwroot/ts/ArticleModule.ts',
-    'theme.css': './wwwroot/css/theme.styl'
+    'zeeko': './wwwroot/ts/Zeeko.ts',
+    'article': './wwwroot/ts/ArticleModule.ts',
+    'theme': './wwwroot/css/theme.styl'
 }
 function createEntries() {
     let result = {};
@@ -30,70 +31,72 @@ let rules = [
     },
     {
         test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-            use: [
-                {
-                    loader: 'css-loader',
-                    options: {
-                        minimize: true,
-                        import: true,
-                        sourceMap: !isProd
-                    }
-                },
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins: [
-                            require('autoprefixer')
-                        ]
-                    }
-                },
-                {
-                    loader: 'stylus-loader',
-                    options: {
-                        preferPathResolver: 'webpack'
-                    }
+        use: [
+            {
+                loader: MiniCssExtractPlugin.loader
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    minimize: true,
+                    import: true,
+                    sourceMap: !isProd
                 }
-            ]
-        })
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: [
+                        require('autoprefixer')
+                    ]
+                }
+            },
+            {
+                loader: 'stylus-loader',
+                options: {
+                    preferPathResolver: 'webpack'
+                }
+            }
+        ]
     }
 ];
-
-let srcMapLoader = {
-    enforce: 'pre',
-    test: /\.ts$/,
-    use: 'source-map-loader'
-};
-
-if (isProd) {
-    rules.push(srcMapLoader);
-}
 
 
 module.exports = {
     entry: createEntries(),
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'commons',
-            filename: 'commons.js'
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
         }),
-        new ExtractTextPlugin({
-            filename: '[name]'
-        }),
-        new MinifyPlugin({},
-            {
-                test: /\.js$/
-            }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions: {
+                map: isProd ? undefined : { inline: true }
+            }
+        })
     ],
     output: {
         path: __dirname + '/wwwroot/dist', //打包后的文件存放的地方
-        filename: '[name]' //打包后输出文件的文件名
+        filename: '[name].js' //打包后输出文件的文件名
     },
     module: {
         rules: rules
     },
     resolve: {
-        extensions: ['.ts', '.css']
+        extensions: ['.ts', '.css', '.styl']
     },
-    devtool: isProd ? '' : 'inline-source-map'
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    test: /\.js$/,
+                    name: 'common',
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        },
+        minimizer: [new TerserPlugin()]
+    },
+    devtool: isProd ? '' : 'inline-source-map',
+    mode: isProd ? 'production' : 'development'
 }
